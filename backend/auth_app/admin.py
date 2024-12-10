0dd80d39ecbba9cb9b7d515724ac226cd3b418b9
Altamash20 <altamash201002@gmail.com>
2024-12-10 14:44:59 +0530

    Register and Login of Users

--- backend/auth_app/admin.py
+++ backend/auth_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from auth_app.models import Books, Review 
 
 # Register your models here.
+admin.site.register(Books)
+admin.site.register(Review)
--- backend/auth_app/models.py
+++ backend/auth_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Books
+# User
+# Reviews
+
+class Books(models.Model):
+    class TypeChoices(models.TextChoices):
+        LITERARY = 'LR', 'Literary'
+        HISTORY = 'HS', 'History'
+        HORROR = 'HR', 'Horror'
+        FICTION = 'FN', 'Fiction'
+        SCIENCE = 'SCN', 'Science'
+        OTHER = 'OTH', 'Other' 
+    Title = models.CharField(max_length=100)
+    Author = models.CharField(max_length=100)
+    ISBN = models.IntegerField()
+    Genre = models.CharField(max_length=100, choices=TypeChoices.choices)
+    Cover_Image_URL = models.URLField(default='')
+
+    def __str__(self):
+        return self.title 
+    
+
+class Review(models.Model):
+    Username = models.ForeignKey(User, on_delete=models.CASCADE)
+    Title = models.ForeignKey(Books, on_delete=models.CASCADE)
+    Rating = models.PositiveSmallIntegerField()
+    Comment = models.CharField(max_length=200)
+
+def __str__(self):
+    return f"Rating: {self.rating}"
+    
