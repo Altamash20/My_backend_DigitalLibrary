1ce0d55a8ed1c0138a9543020d919bf2c95e5fa5
Mohammad Altamash <altamash201002@gmail.com>
2025-03-16 13:27:38 +0530

    nothing modified

--- backend/auth_app/admin.py
+++ backend/auth_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from auth_app.models import Book, Review 
+from auth_app.models import Book, Review
 
 # Register your models here.
 admin.site.register(Book)
 admin.site.register(Review)
+# admin.site.register(User) , model user is already registered, no need to register again here else gives error
